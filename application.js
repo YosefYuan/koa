bce3cb2c19b3abccfbb930666eb924eff993759c
yosef yuan <yuanruixiangjet@163.com>
2018-10-23 13:19:05 +0800

    update handleRequest in application

--- application.js
+++ application.js
@@ -1,5 +1,6 @@
 let http = require('http')
 let EventEmitter = require('events')
+let Stream = require('stream')
 let context = require('./context')
 let request = require('./request')
 let response = require('./response')
@@ -32,9 +33,21 @@ class Koa extends EventEmitter {
     }
 
     handleRequest(req, res) {
+        res.statusCode = 404
         let ctx = this.createContext(req, res)
         this.fn(ctx)
-        res.end(ctx.body)
+        if (typeof ctx.body == 'object') {
+            res.setHeader('Content-Type', 'application/json;charset=utf8')
+            res.end(JSON.stringify(ctx.body))
+        } else if (ctx.body instanceof Stream) {
+            ctx.body.pipe(res)
+        }
+        else if (typeof ctx.body === 'string' || Buffer.isBuffer(ctx.body)) {
+            res.setHeader('Content-Type', 'text/htmlcharset=utf8')
+            res.end(ctx.body)
+        }else{
+            res.end('Not found')
+        }
     }
 
     listen(...args) {
